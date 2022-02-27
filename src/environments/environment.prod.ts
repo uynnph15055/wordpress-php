9f7c3f0911f5d435e18072534c8f54cecdb6a817
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-02-27 23:44:23 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const baseApiUrl = "http://localhost:8000/api";
+const authApi =  `${baseApiUrl}/auth`;
+export const jwtApiUrl = `${baseApiUrl}/v1`; 
+const publicApiUrl = `${baseApiUrl}/public`;
 export const environment = {
-  production: true
+  production: false,
+  GG_CLIENT_ID: "848473416580-mn36tpmh8gpm72qp1rqccjii92829bk2.apps.googleusercontent.com",
+  GG_CLIENT_SECRET: "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",
+  loginUrl: authApi + "/login-token",
+  userListUrl: `${jwtApiUrl}/users`,
+  sponsorListUrl: `${publicApiUrl}/sponsors`
 };
