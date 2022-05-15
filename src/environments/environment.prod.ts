c5334418464bb210d324c2bfa739fe17a25a5017
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-05-15 22:30:25 +0700

    Update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,15 +4,18 @@ const authApi = `${baseApiUrl}/auth`;
 export const jwtApiUrl = `${baseApiUrl}/v1`;
 const publicApiUrl = `${baseApiUrl}/public`;
 export const environment = {
-  production: false,
+  production: true,
   GG_CLIENT_ID: "437095088104-c58gonumb2mu71c1d21ofn6ita2uvqr5.apps.googleusercontent.com",
   GG_CLIENT_SECRET: "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",
   loginUrl: authApi + "/login-token",
   userListUrl: `${jwtApiUrl}/users`,
   sponsorListUrl: `${publicApiUrl}/sponsors`,
   contestListUrl: `${publicApiUrl}/contests`,
+  contestV1Url: `${jwtApiUrl}/contest`,
   majorListUrl: `${publicApiUrl}/majors`,
   roundListUrl: `${publicApiUrl}/rounds`,
+  roundV1Url: `${jwtApiUrl}/round`,
   sliderListUrl: `${publicApiUrl}/sliders`,
   teamListUrl: `${jwtApiUrl}/teams`,
+  takeExamUrl: `${jwtApiUrl}/take-exam`
 };
