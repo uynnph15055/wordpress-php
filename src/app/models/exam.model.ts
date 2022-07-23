36b7c6a2d8d6aa3253b14f501f921e7ce7c85a28
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-07-23 10:41:10 +0700

    SV làm bài thi và check trạng thái làm bài

--- src/app/models/exam.model.ts
+++ src/app/models/exam.model.ts
@@ -1,3 +1,19 @@
 export class Exam {
     id: number;
 }
+
+export class ExamCapacity {
+    questions: {
+        id: number,
+        content: string,
+        answers: {
+            id: number,
+            content: string
+        }[]
+    }[];
+    time: number;
+    time_type: number;
+    exam_at: Date;
+    name: string;
+    time_exam: number; // thời gian làm bài (phút)
+}
--- src/app/pages/capacity-exam/capacity-exam.component.ts
+++ src/app/pages/capacity-exam/capacity-exam.component.ts
@@ -9,6 +9,8 @@ import { Round } from 'src/app/models/round.model';
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from 'src/app/services/user.service';
 import { DOCUMENT } from '@angular/common';
+import { User } from 'src/app/models/user';
+import { ExamCapacity } from 'src/app/models/exam.model';
 
 @Component({
   selector: 'app-capacity-exam',
@@ -18,8 +20,9 @@ import { DOCUMENT } from '@angular/common';
 export class CapacityExamComponent implements OnInit {
 
   @ViewChildren("questions") questions: QueryList<ElementRef>;
+  userLogged!: User;
   formAnswers!: FormGroup;
-  fakeQuestionData!: any;
+  examData!: ExamCapacity;
   // DS id câu hỏi đã trả lời
   questionListId: { questionId: number }[] = [];
   // trạng thái làm bài: 0 -> màn hình chờ làm bài, 1 -> đang làm bài, 2 -> đã nộp
@@ -34,6 +37,8 @@ export class CapacityExamComponent implements OnInit {
   isNotiExamTimeOut = false;
   // trạng thái đang call api nộp bài
   isSubmitingExam = false;
+  // trạng thái hết giờ làm bài
+  isTimeOut = false;
   timerId!: any;
   element!: any;
 
@@ -62,12 +67,29 @@ export class CapacityExamComponent implements OnInit {
     this.route.paramMap.pipe(
       map(params => params.get('round_id')),
       switchMap(id => this.roundService.getRoundWhereId(id))
-    ).subscribe(res => {
-      if (res.status) {
+    ).subscribe(responseRound => {
+      if (responseRound.status) {
         this.isFetchingRound = false;
-        this.roundDetail = res.payload;
-        this.roundDetail.start_time = new Date("2022-06-25 15:25:54")
-        console.log(res.payload)
+        this.roundDetail = responseRound.payload;
+        console.log(responseRound)
+
+        // check login
+        this.userLogged = this.userService.getUserValue();
+        // nếu đã login ? check trạng thái làm bài : hiển thị màn hình chờ làm bài
+        if (this.userLogged.id) {
+          this.roundService.getInfoCapacityExamRound({
+            round_id: responseRound.payload.id
+          }).subscribe(resSttExam => {
+            // nếu đang làm ? tiếp tục làm bài
+            if (resSttExam.status && resSttExam.payload === 0) {
+              
+              this.takeExam();
+              console.log(resSttExam)
+            } else if (resSttExam.status && resSttExam.payload === 1) {
+              // đã nộp bài
+            }
+          })
+        }
       }
     })
   }
@@ -97,80 +119,95 @@ export class CapacityExamComponent implements OnInit {
         // check thời gian thi
         const todayTime = new Date().getTime();
         const timeStart = new Date(this.roundDetail.start_time).getTime();
+        const timeEnd = new Date(this.roundDetail.end_time).getTime();
 
         if (todayTime < timeStart) {
           this.toast.warning({ summary: "Chưa đến thời gian làm bài"});
           return;
         }
-
-        // fake api tạo bản nháp
-        const res = this.roundService.getInfoCapacityExamRound();
-        if (res.status) {
-          const data = res.payload;
-
-          // bật toàn màn hình
-          this.openFullscreen();
-
-          setTimeout(() => {
-            // kích thước khi full màn hình
-            this.windowScreenSize = {
-              width: window.innerWidth,
-              height: window.innerHeight
-            }
-
-            // cập nhật trạng thái đang làm bài
-            this.statusTakingExam = 1;
-
-            this.fakeQuestionData = data.questions;
-            this.createFormControl();
-
-            const durationExam = data.time_type === 1 ? (data.time * 60) : data.time;
-            this.handleStartExam(durationExam, data.created_at);
-
-            // bắt sự kiện thay đổi kích thước màn hình (f11)
-            window.onresize = (e: any) => {
-              const currentWindowWidth = e.target.innerWidth;
-              const currentWindowHeight = e.target.innerHeight;
-
-              const { width, height } = this.windowScreenSize;
-
-              if (currentWindowWidth !== width || currentWindowHeight !== height) {
-                this.dialog.closeAll();
-
-                const dialogFullscreen = this.dialog.open(DialogConfirmComponent, {
-                  width: '300px',
-                  disableClose: true,
-                  data: {
-                    isNotShowBtnCancel: true,
-                    title: "Cảnh báo",
-                    description: "Vui lòng bật full màn hình khi làm bài!"
-                  }
-                });
-
-                dialogFullscreen.afterClosed().subscribe(result => {
-                  result === "true" && this.openFullscreen();
-                })
-              } else {
-                this.dialog.closeAll();
-              }
-            }
-
-            // chặn f12
-            window.onkeydown = (e: any) => this.handleDisableKeydown(e);
-
-            // chặn chuột phải
-            window.oncontextmenu = (e: any) => this.disabledEvent(e);
-          }, 100)
+        if (todayTime >= timeEnd) {
+          this.toast.warning({ summary: "Phần thi đã kết thúc"});
+          return;
         }
+
+        this.takeExam();
+      }
+    })
+  }
+
+  takeExam() {
+    this.roundService.takeExam({round_id: this.roundDetail.id}).subscribe(res => {
+      if (res.status) {
+        this.examData = {
+          ...res.payload,
+          exam_at: res.exam_at,
+          time_exam: this.convertTimeExamToSeconds(res.payload.time, res.payload.time_type) / 60
+        };
+
+        // bật toàn màn hình
+        this.openFullscreen();
+
+        setTimeout(() => {
+          // kích thước khi full màn hình
+          this.windowScreenSize = {
+            width: window.innerWidth,
+            height: window.innerHeight
+          }
+
+          // cập nhật trạng thái đang làm bài
+          this.statusTakingExam = 1;
+
+          this.createFormControl();
+
+          const durationExam = this.convertTimeExamToSeconds(this.examData.time, this.examData.time_type);
+          this.handleStartExam(durationExam, this.examData.exam_at);
+
+          // bắt sự kiện thay đổi kích thước màn hình (f11)
+          window.onresize = (e: any) => {
+            if (this.isTimeOut) return;
+            
+            const currentWindowWidth = e.target.innerWidth;
+            const currentWindowHeight = e.target.innerHeight;
+
+            const { width, height } = this.windowScreenSize;
+
+            if (currentWindowWidth !== width || currentWindowHeight !== height) {
+              this.dialog.closeAll();
+
+              const dialogFullscreen = this.dialog.open(DialogConfirmComponent, {
+                width: '300px',
+                disableClose: true,
+                data: {
+                  isNotShowBtnCancel: true,
+                  title: "Cảnh báo",
+                  description: "Vui lòng bật full màn hình khi làm bài!"
+                }
+              });
+
+              dialogFullscreen.afterClosed().subscribe(result => {
+                result === "true" && this.openFullscreen();
+              })
+            } else {
+              this.dialog.closeAll();
+            }
+          }
+
+          // chặn f12
+          window.onkeydown = (e: any) => this.handleDisableKeydown(e);
+
+          // chặn chuột phải
+          window.oncontextmenu = (e: any) => this.disabledEvent(e);
+        }, 100)
+        console.log(res)
       }
     })
   }
 
   // nộp bài
   handleSubmitExam() {
+    const answersData = this.getAnswersData();
     // check làm thiếu câu hỏi
     if (this.formAnswers.valid) {
-      const answersData = this.getAnswersData();
 
       const confirmSubmitExam = this.dialog.open(DialogConfirmComponent, {
         disableClose: true,
@@ -208,6 +245,7 @@ export class CapacityExamComponent implements OnInit {
       confirmSubmitExam.afterClosed().subscribe(result => {
         // xác nhận nộp bài
         if (result === "true") {
+          console.log(answersData)
           this.openDialogSubmitExam();
           this.submitExam();
         }
@@ -249,7 +287,7 @@ export class CapacityExamComponent implements OnInit {
 
   createFormControl() {
     const ctrls: { [name: string ]: FormControl } = {};
-    this.fakeQuestionData.forEach((question: any, index: number) => {
+    this.examData.questions.forEach((question: any, index: number) => {
       const fieldName = `question-${++index}-${question.id}`;
       ctrls[fieldName] = new FormControl('', Validators.required)
     })
@@ -303,10 +341,9 @@ export class CapacityExamComponent implements OnInit {
   // bắt đầu làm bài
   handleStartExam(duration: number, timeStart: any) {
     // tính thời gian làm bài ban đầu
-    const minutesExam = Math.floor(((duration % (60 * 60 * 24)) % (60 * 60)) / 60 );
-    const secondsExam = Math.floor(((duration % (60 * 60 * 24)) % (60 * 60)) % 60 );
-    this.countDownTimeExam.minutes = minutesExam < 10 ? `0${minutesExam}` : minutesExam;
-    this.countDownTimeExam.seconds = secondsExam < 10 ? `0${secondsExam}` : secondsExam;
+    const { minutes: minutesExam, seconds: secondsExam } = this.convertMsToMinutesAndSecond(duration * 1000);
+    this.countDownTimeExam.minutes = minutesExam;
+    this.countDownTimeExam.seconds = secondsExam;
 
     let timeStartExam: any = new Date(timeStart).getTime();
     const timeWillEndExam = new Date(timeStartExam + duration * 1000 + 1000);
@@ -314,13 +351,16 @@ export class CapacityExamComponent implements OnInit {
     this.timerId = setInterval(() => {
       let futureDate = new Date(timeWillEndExam).getTime();
       let today = new Date().getTime();
+      let timeEndRound = new Date(this.roundDetail.end_time).getTime();
 
       let distance = futureDate - today;
 
-      if (distance < 0) {
+      // hết giờ hoặc vòng thi đóng
+      if (distance < 0 || today > timeEndRound) {
         this.countDownTimeExam.minutes = "00";
         this.countDownTimeExam.seconds = "00";
         clearInterval(this.timerId);
+        this.isTimeOut = true;
 
         // nếu đang không nộp bài => tự động nộp bài
         if (!this.isSubmitingExam) {
@@ -340,6 +380,8 @@ export class CapacityExamComponent implements OnInit {
 
           submitExamRef.afterClosed().subscribe(result => {
             if (result === "true") {
+              const answersData = this.getAnswersData();
+              console.log(answersData)
               this.openDialogSubmitExam();
 
               this.submitExam();
@@ -347,11 +389,9 @@ export class CapacityExamComponent implements OnInit {
           })
         }
       } else {
-        const minutes: string | number = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        this.countDownTimeExam.minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-        const seconds: number | string = Math.floor((distance % (1000 * 60)) / 1000);
-        this.countDownTimeExam.seconds = seconds < 10 ? `0${seconds}` : seconds;
+        const { minutes, seconds } = this.convertMsToMinutesAndSecond(distance);
+        this.countDownTimeExam.minutes = minutes;
+        this.countDownTimeExam.seconds = seconds;
 
         // thông báo sắp hết giờ
         if (minutes <= 1 && !this.isNotiExamTimeOut) {
@@ -362,6 +402,17 @@ export class CapacityExamComponent implements OnInit {
 
       console.log(this.countDownTimeExam.minutes, this.countDownTimeExam.seconds)
     }, 1000);
+  }
+
+  // convert milisecond to minus
+  convertMsToMinutesAndSecond(milisecond: number) {
+    let minutes: string | number = Math.floor(milisecond / 60000);
+    let seconds: string | number = +((milisecond % 60000) / 1000).toFixed(0);
+
+    if (minutes < 10) minutes = `0${minutes}`;
+    if (seconds < 10) seconds = `0${seconds}`;
+
+    return { minutes, seconds };
   }
 
   openDialogSubmitExam() {
@@ -480,5 +531,25 @@ export class CapacityExamComponent implements OnInit {
       this.disabledEvent(e);
       navigator.clipboard.writeText("Thí sinh không được gian lận trong quá trình làm bài!");
     }
+  }
+
+  // get thời gian làm bài
+  convertTimeExamToSeconds(time: number, time_type: number) {
+    // time_type: 0 => phút, 1 => giờ, 2 => ngày
+    let timeExam = 0;
+
+    switch(time_type) {
+      case 0: 
+        timeExam = time * 60;
+        break;
+      case 1:
+        timeExam = time * 60 * 60
+        break;
+      case 2:
+        timeExam = time * 24 * 60 * 60
+        break;
+    }
+
+    return timeExam;
   }
 }
