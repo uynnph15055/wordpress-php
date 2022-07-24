449941e71bbb3f05cd4293b19cc717b4a50cac04
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-07-24 15:19:55 +0700

    Update base api url admin.svpoly.breezecandles.vn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,15 @@
-const baseApiUrl = "https://admin.svpoly.xyz/api";
+// const baseApiUrl = "https://admin.svpoly.xyz/api";
+const baseApiUrl = "http://admin.svpoly.breezecandles.vn/api";
 const authApi = `${baseApiUrl}/auth`;
 
 export const jwtApiUrl = `${baseApiUrl}/v1`;
 const publicApiUrl = `${baseApiUrl}/public`;
 export const environment = {
   production: true,
-  GG_CLIENT_ID: "437095088104-c58gonumb2mu71c1d21ofn6ita2uvqr5.apps.googleusercontent.com",
-  GG_CLIENT_SECRET: "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",
+  // GG_CLIENT_ID: "437095088104-c58gonumb2mu71c1d21ofn6ita2uvqr5.apps.googleusercontent.com",
+  // GG_CLIENT_SECRET: "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",
+  GG_CLIENT_ID: "723333154556-dbmchqand5vseqtfhskvdo121stiohqd.apps.googleusercontent.com", // tuannda3 config
+  GG_CLIENT_SECRET: "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", // tuannda3 config
   loginUrl: authApi + "/login-token",
   publicApiUrl: `${baseApiUrl}/public`,
   sponsorListUrl: `${publicApiUrl}/sponsors`,
