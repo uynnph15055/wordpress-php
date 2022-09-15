85263ce693986c324880b598f5b40f174111c714
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-09-15 19:58:50 +0700

    render lại dữ liệu khi click chuyển Tab danh mục

--- src/app/pages/post-category/post-category.component.ts
+++ src/app/pages/post-category/post-category.component.ts
@@ -8,23 +8,12 @@ import { ListPostService } from 'src/app/services/list-post.service';
   styleUrls: ['./post-category.component.css']
 })
 export class PostCategoryComponent implements OnInit {
-  ListPost: Post[]
+  ListPost: Post[] | null
 
-   PostRecruitmentFirst : Post; 
-  ListPostRecruitment: Post[]
-
-  PostContestFirst : Post; 
-  ListPostContest: Post[]
-
-  PostCapacityFirst : Post; 
-  ListPostCapacity: Post[]
 
   constructor(private postService : ListPostService) { }
 
   ngOnInit(): void {
-    this.getListPostRecruitment();
-    this.getListPostCapacity()
-    this.getListPostContest()
     this.getListPost()
   }
 
@@ -35,60 +24,14 @@ export class PostCategoryComponent implements OnInit {
        }
     })
   }
-
-  getListPostRecruitment(){
-    this.postService.getPostRecruitment().subscribe(res => {
-       if(res.status){
-        let arrResult= res.payload.data;
-        this.PostRecruitmentFirst = arrResult[0];
-        // let indexRan = Math.floor(Math.random() * arrResult.length ) +1;
-        // this.postSeccond = arrResult[indexRan];
-        this.ListPostRecruitment = arrResult.filter((res: Post , index: number) => {
-          return index > 0 && index < 3;
-        });
-       }
-    })
+  onChangeDataPost(data: string){
+    this.ListPost = null
+        this.postService.getPostByCategory(data).subscribe(res => {
+        if(res.status){
+          this.ListPost = res.payload.data
+        }
+      })
+   
   }
-
-  getListPostContest(){
-    this.postService.getPostContest().subscribe(res => {
-       if(res.status){
-        let arrResult= res.payload.data;
-        this.PostContestFirst = arrResult[0];
-        // let indexRan = Math.floor(Math.random() * arrResult.length ) +1;
-        // this.postSeccond = arrResult[indexRan];
-        this.ListPostContest = arrResult.filter((res: Post , index: number) => {
-          return index > 0;
-        });
-       }
-    })
-  }
-
-  getListPostCapacity(){
-    this.postService.getPostCapacity().subscribe(res => {
-       if(res.status){
-        let arrResult= res.payload.data;
-        this.PostCapacityFirst = arrResult[0];
-        // let indexRan = Math.floor(Math.random() * arrResult.length ) +1;
-        // this.postSeccond = arrResult[indexRan];
-        this.ListPostCapacity = arrResult.filter((res: Post , index: number) => {
-           return index > 0 && index < 4;
-        });
-       }
-    })
-  }
-
-  getPostWhereCate(cate: string ,elementString: any, distanceApart: number){
-    let element = document.querySelector(elementString);
-    let numberScroll = element.offsetTop;
-    window.scrollTo({ top: numberScroll - distanceApart, behavior: 'smooth' });
-    this.postService.getPostWhereCate(cate).subscribe(res => {
-      console.log(res.payload.data);
-    });
-  }
-
-  sliderHeaderPost = {
-    "slidesToShow": 2 , infinite: true, autoplay: true, arrows: true, prevArrow: '.prev-arrow', nextArrow: '.next-arrow', slidesToScroll: 1, fadeSpeed: 1000,
-  }
-
+ 
 }
--- src/app/services/list-post.service.ts
+++ src/app/services/list-post.service.ts
@@ -33,5 +33,8 @@ export class ListPostService {
   getPostCapacity() : Observable<ResponsePayload>{
     return this.http.get<ResponsePayload>(`${environment.postListUrl}?post=post-capacity`);
   }
+  getPostByCategory(data: string) : Observable<ResponsePayload>{
+    return this.http.get<ResponsePayload>(`${environment.postListUrl}?post=${data}`);
+  }
   
 }
