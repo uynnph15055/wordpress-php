dcf580495937542169197120ba39e354fab8a213
Link <duyphi456@gmail.com>
2022-09-24 20:57:11 +0700

    Fix Filter capacity

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // https://admin.svpoly.breezecandles.vn/api
 // http://127.0.0.1:8000/api
-// const baseApiUrl = "http://127.0.0.1:8000/api";
-const baseApiUrl = "https://admin.svpoly.breezecandles.vn/api";
+const baseApiUrl = "http://127.0.0.1:8000/api";
+// const baseApiUrl = "https://admin.svpoly.breezecandles.vn/api";
 const authApi = `${baseApiUrl}/auth`;
 export const jwtApiUrl = `${baseApiUrl}/v1`;
 export const publicApiUrl = `${baseApiUrl}/public`;
