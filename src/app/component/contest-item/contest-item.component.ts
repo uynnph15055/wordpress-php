01bf940a7c97c9870dce786598d1ea421fa90203
Uy nguyễn <88134149+uynnph15055@users.noreply.github.com>
2022-10-21 12:11:48 +0700

    Update

--- src/app/component/contest-item/contest-item.component.ts
+++ src/app/component/contest-item/contest-item.component.ts
@@ -6,6 +6,7 @@ import { UserService } from 'src/app/services/user.service';
 import { WishlistService } from 'src/app/services/wishlist.service';
 import { Post } from 'src/app/models/post.model';
 import { NgToastService } from 'ng-angular-popup';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contest-item',
@@ -35,7 +36,7 @@ export class ContestItemComponent implements OnInit {
   minutes: number;
   seconds: number;
 
-  constructor(private userService: UserService ,   private toast: NgToastService ,  private wishlist : WishlistService) {}
+  constructor(private userService: UserService , private router: Router,   private toast: NgToastService ,  private wishlist : WishlistService) {}
  
   ngOnInit(): void {
     this.userService.getUserValue().id != undefined
@@ -117,26 +118,32 @@ export class ContestItemComponent implements OnInit {
       id: item.id,
     }
 
-    this.requestFavorite.emit(item);
-    if(item.user_wishlist){
-      event.currentTarget.classList.remove('primary-color');
-      event.currentTarget.parentElement.classList.remove('opacity-100');
-      event.currentTarget.parentElement.classList.add('my-add-favorite__icon');
-      this.wishlist.wishListRemove(data).subscribe(res => {
-        if(res.status){
-          this.toast.success({ summary: res.payload, duration: 2000 });
-        }
-      });
+    if(!this.userService.getUserValue()){
+      this.router.navigate(['/login']);
     }else{
-      event.currentTarget.classList.add('primary-color');
-      event.currentTarget.parentElement.classList.add('opacity-100');
-      event.currentTarget.parentElement.classList.remove('my-add-favorite__icon');
-      this.wishlist.wishListAdd(data).subscribe(res => {
-        if(res.status){
-          this.toast.success({ summary: res.payload, duration: 2000 });
-        }
-      });
+      this.requestFavorite.emit(item);
+      if(item.user_wishlist){
+        event.currentTarget.classList.remove('primary-color');
+        event.currentTarget.parentElement.classList.remove('opacity-100');
+        event.currentTarget.parentElement.classList.add('my-add-favorite__icon');
+        this.wishlist.wishListRemove(data).subscribe(res => {
+          if(res.status){
+            this.toast.success({ summary: res.payload, duration: 2000 });
+          }
+        });
+      }else{
+        event.currentTarget.classList.add('primary-color');
+        event.currentTarget.parentElement.classList.add('opacity-100');
+        event.currentTarget.parentElement.classList.remove('my-add-favorite__icon');
+        this.wishlist.wishListAdd(data).subscribe(res => {
+          if(res.status){
+            this.toast.success({ summary: res.payload, duration: 2000 });
+          }
+        });
+      }
     }
+
+   
   }
 
 
--- src/app/component/home/recruitment-position/recruitment-position.component.ts
+++ src/app/component/home/recruitment-position/recruitment-position.component.ts
@@ -1,46 +1,60 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { Contest } from 'src/app/models/contest';
 import { Post } from 'src/app/models/post.model';
+import { UserService } from 'src/app/services/user.service';
 import { WishlistService } from 'src/app/services/wishlist.service';
 
 @Component({
   selector: 'app-recruitment-position',
   templateUrl: './recruitment-position.component.html',
-  styleUrls: ['./recruitment-position.component.css']
+  styleUrls: ['./recruitment-position.component.css'],
 })
 export class RecruitmentPositionComponent implements OnInit {
   @Input() item: Post;
   @Output() requestFavorite = new EventEmitter<Post>();
-  constructor(private wishlist: WishlistService ,  private toast: NgToastService) { }
+  constructor(
+    private userService: UserService,
+    private wishlist: WishlistService,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  favoriteEvent(event: any , item: Post){
+  favoriteEvent(event: any, item: Post) {
     const data = {
-      type : 'post',
+      type: 'post',
       id: item.id,
-    }
-    this.requestFavorite.emit(item);
-    if(item.user_wishlist){
-      event.currentTarget.classList.remove('primary-color');
-      event.currentTarget.parentElement.classList.remove('opacity-100');
-      event.currentTarget.parentElement.classList.add('my-add-favorite__icon');
-      this.wishlist.wishListRemove(data).subscribe(res => {
-        if(res.status){
-          this.toast.success({ summary: res.payload, duration: 2000 });
-        }
-      });
-    }else{
-      event.currentTarget.classList.add('primary-color');
-      event.currentTarget.parentElement.classList.add('opacity-100');
-      event.currentTarget.parentElement.classList.remove('my-add-favorite__icon');
-      this.wishlist.wishListAdd(data).subscribe(res => {
-        if(res.status){
-          this.toast.success({ summary: res.payload, duration: 2000 });
-        }
-      });
+    };
+    if (!this.userService.getUserValue()) {
+      this.router.navigate(['/login']);
+    } else {
+      this.requestFavorite.emit(item);
+      if (item.user_wishlist) {
+        event.currentTarget.classList.remove('primary-color');
+        event.currentTarget.parentElement.classList.remove('opacity-100');
+        event.currentTarget.parentElement.classList.add(
+          'my-add-favorite__icon'
+        );
+        this.wishlist.wishListRemove(data).subscribe((res) => {
+          if (res.status) {
+            this.toast.success({ summary: res.payload, duration: 2000 });
+          }
+        });
+      } else {
+        event.currentTarget.classList.add('primary-color');
+        event.currentTarget.parentElement.classList.add('opacity-100');
+        event.currentTarget.parentElement.classList.remove(
+          'my-add-favorite__icon'
+        );
+        this.wishlist.wishListAdd(data).subscribe((res) => {
+          if (res.status) {
+            this.toast.success({ summary: res.payload, duration: 2000 });
+          }
+        });
+      }
     }
   }
 }
